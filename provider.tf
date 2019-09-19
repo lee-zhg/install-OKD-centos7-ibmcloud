b69d1b940944a0b774438911c12e9c88b2a59d9b
lee-zhg <zhangllc@outlook.com>
2019-09-19 15:58:10 -0500

    Add files via upload

--- provider.tf
+++ provider.tf
@@ -1,11 +1,5 @@
-# variables supplied from terraform.tfvars
-
-variable "iaas_username" {}
-variable "ibmcloud_iaas_api_key" {}
-variable "ibmcloud_api_key" {}
-
 provider "ibm" {
-  softlayer_username = "${var.iaas_username}"
-  softlayer_api_key  = "${var.ibmcloud_iaas_api_key}"
+  softlayer_username = "${var.ibm_sl_username}"
+  softlayer_api_key  = "${var.ibm_sl_api_key}"
   bluemix_api_key    = "${var.ibmcloud_api_key}"
 }
